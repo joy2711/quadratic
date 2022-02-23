ae7cae05c52522b7463c15c627788bc0929c9d3c
Joy Anne Wairimu <60652642+joy2711@users.noreply.github.com>
2022-02-23 12:31:52 +0300

    Add files via upload
    Rough idea of what the program should do.

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+int main()
+{
+    float a,b,c,d;
+    float r1,r2;
+    float rpart,den;
+
+    printf("Enter coefficients of x (a,b and c)\n");
+    scanf("%f%f%f", &a, &b, &c);
+    rpart = sqrt(b*b - 4 *a *c);
+    den = 2 * a;
+    r1 = (-b + rpart) /den;
+    r2 = (-b - rpart) /den;
+    d = (b * b - 4 * a * c);
+
+    if (d == 0){
+    printf("Equation has two values ; %d\n%d\n",r1,r2);
+    }
+
+    else if (d > 0){
+        printf("Equation has two distinct values ; %d\n%d\n",r1,r2);
+    }
+    if (d < 0){
+        printf("Equation has no roots");
+    }
+
+
+    return 0;
+}
